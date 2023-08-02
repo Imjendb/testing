d8001a19007785b6c2abc57f10304cc57e9091e0
i.jendoubi <i.jendoubi@instadeep.com>
2023-08-02 14:06:39 +0100

    feat: compute performance on model slices

--- starter/starter/ml/model.py
+++ starter/starter/ml/model.py
@@ -1,5 +1,6 @@
 from sklearn.metrics import fbeta_score, precision_score, recall_score
 from sklearn.linear_model import LogisticRegression
+from ml.data import process_data
 
 # Optional: implement hyperparameter tuning.
 def train_model(X_train, y_train,max_iter=1000, random_state=23):
@@ -43,6 +44,32 @@ def compute_model_metrics(y, preds):
     recall = recall_score(y, preds, zero_division=1)
     return precision, recall, fbeta
 
+def performance_on_slide(feature,cls,data, model,cat_features,encoder,lb):
+    """ computes model performance on slice of data.
+
+    Inputs
+    ------
+    feature: the exact feature among categorical feature to consider
+    cls : the fixed value of a the given feature 
+    data: data on which perform data slicing
+    model
+        Trained machine learning model.
+    cat_features: list[str]
+        List containing the names of the categorical features (default=[])
+    encoder: sklearn.preprocessing._encoders.OneHotEncoder
+        Trained sklearn OneHotEncoder
+    lb : sklearn.preprocessing._label.LabelBinarizer
+        Trained sklearn LabelBinarizer
+    Returns
+    -------
+    fbeta : float
+    precision : float
+    recall : float
+    """
+    slice1=data[data[feature] == cls]
+    X_val, y_val, encoder_, lb_ = process_data(slice1, categorical_features=cat_features, label="salary", training=False,encoder=encoder,lb=lb)
+    fbeta,precision,recall=compute_model_metrics(y_val,inference(model,X_val))
+    return fbeta,precision,recall
 
 def inference(model, X):
     """ Run model inferences and return the predictions.
@@ -58,4 +85,4 @@ def inference(model, X):
     preds : np.array
         Predictions from the model.
     """
-    pass
+    return model.predict(X)
--- starter/starter/train_model.py
+++ starter/starter/train_model.py
@@ -4,15 +4,39 @@ import pathlib
 import pandas as pd
 from ml.data import process_data
 from ml.model import train_model
+from ml.model import compute_model_metrics
+from ml.model import inference
 import pickle
 import pandas as pd
+import numpy as np
+import os
+from ml.model import performance_on_slide
 
-#import data
 data_directory=pathlib.Path(os.path.abspath('__file__')).parent/"starter"/"data"
+slices_path=pathlib.Path(os.path.abspath('__file__')).parent/"starter"
+model_path=pathlib.Path(os.path.abspath('__file__')).parent/"starter"/"model"/"trained_model.pickle"
+
 data = pd.read_csv(data_directory/"census.csv")
 
+# replace "?" with nan
+data.replace("?", np.nan, inplace=True)
+
+# replace the null values in the columns with missing values 
+# In particular, the nan values are replaces with the value that has the highest occurence
+high_occ_workclass = data['workclass'].value_counts().idxmax()
+high_occ_occupation = data['occupation'].value_counts().idxmax()
+high_occ_country = data['native-country'].value_counts().idxmax()
+
+nan_columns = ['workclass', 'occupation', 'native-country']
+high_occ = [high_occ_workclass, high_occ_occupation, high_occ_country]
+for i in range(len(nan_columns)):
+    data[nan_columns[i]].fillna(high_occ[i], inplace=True)
+
+print('Dataset columns with null values:\n', data.isnull().sum())
+
+
 # split data into training and testing sets
-train, test = train_test_split(data, test_size=0.20)
+train, test = train_test_split(data, test_size=0.20, random_state=23)
 
 
 cat_features = [
@@ -30,8 +54,31 @@ X_train, y_train, encoder, lb = process_data(
     train, categorical_features=cat_features, label="salary", training=True
 )
 # train model
-model=train_model(X_train, y_train,max_iter=1000, random_state=23)
-model_path=data_directory=pathlib.Path(os.path.abspath('__file__')).parent/"starter"/"model"/"trained_model.pickle"
+model=train_model(X_train, y_train,max_iter=10000, random_state=23)
 
 # save model
-#pickle.dump(model, open(model_path, "wb"))
+pickle.dump(model, open(model_path, "wb"))
+print('training ends')
+print('start evaluation--overall performance--')
+X_val, y_val, encoder_, lb_ = process_data(test, categorical_features=cat_features, label="salary", training=False,encoder=encoder,lb=lb)
+fbeta,precision,recall=compute_model_metrics(y_val,inference(model,X_val))
+print("fbeta:",fbeta)
+print("precision:",precision)
+print("recall:",recall)
+print('------------------------------------------------')
+
+with open(slices_path/"slice_output.txt",'w') as f:    
+    print('Evaluation--data slicing--',file=f)
+    for feature in cat_features:
+        print(f"###############################################",file=f)
+        print(f"############### Feature: {feature}  ###############",file=f)
+        print(f"###############################################",file=f)
+        for cls in test[feature].unique():
+            fbeta,precision,recall=performance_on_slide(feature,cls,test, model,cat_features,encoder,lb)
+            print(f"fbeta on {feature} = {cls} slices:",file=f)
+            print(fbeta,file=f)
+            print(f"precision on {feature} = {cls} slices:",file=f)
+            print(precision,file=f)
+            print(f"recall on {feature} = {cls} slices:",file=f)
+            print(recall,file=f)
+            print('------------------------------------------------',file=f)
